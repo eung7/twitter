129b18d6db2e5eb91d33e6ebe1934afcaad86202
eung7 <wannabeeung@gmail.com>
2022-05-28 16:09:06 +0900

    First Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by 김응철 on 2022/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
